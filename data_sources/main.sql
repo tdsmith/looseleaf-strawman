953d9d21e2ed5f34c1e409afba4c85cc8b792158
Tim Smith <tdsmith@mozilla.com>
2019-11-13 13:01:22 -0800

    Fix boundary condition

--- data_sources/main.sql
+++ data_sources/main.sql
@@ -10,8 +10,8 @@ FROM
   RIGHT INNER JOIN {{source.normandy_enrollments}} ON (
     {{source.normandy_enrollments}}.client_id = main.client_id
     AND DATE(main.submission_timestamp) >= DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.start}} DAY)
-    AND DATE(main.submission_timestamp) <= DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.end}} DAY)
+    AND DATE(main.submission_timestamp) < DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.end}} DAY)
   )
 WHERE
   DATE(main.submission_timestamp) >= DATE_ADD({{experiment.start}}, {{window.start}} DAY)
-  AND DATE(main.submission_timestamp) <= DATE_ADD({{experiment.start}}, {{window.end}} DAY)
+  AND DATE(main.submission_timestamp) < DATE_ADD({{experiment.enrollment_end}}, {{window.end}} DAY)
