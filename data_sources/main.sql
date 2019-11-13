b09a25348545767a9395be17ffadde6db875b4e4
Tim Smith <tdsmith@mozilla.com>
2019-11-13 12:58:33 -0800

    Fully reference columns

--- data_sources/main.sql
+++ data_sources/main.sql
@@ -9,9 +9,9 @@ FROM
   `moz-fx-data-shared-prod.telemetry.main`
   RIGHT INNER JOIN {{source.normandy_enrollments}} ON (
     {{source.normandy_enrollments}}.client_id = main.client_id
-    AND DATE(submission_timestamp) >= DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.start}} DAY)
-    AND DATE(submission_timestamp) <= DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.end}} DAY)
+    AND DATE(main.submission_timestamp) >= DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.start}} DAY)
+    AND DATE(main.submission_timestamp) <= DATE_ADD({{source.normandy_enrollments}}.enrollment_date, {{window.end}} DAY)
   )
 WHERE
-  DATE(submission_timestamp) >= DATE_ADD({{experiment.start}}, {{window.start}} DAY)
-  AND DATE(submission_timestamp) <= DATE_ADD({{experiment.start}}, {{window.end}} DAY)
+  DATE(main.submission_timestamp) >= DATE_ADD({{experiment.start}}, {{window.start}} DAY)
+  AND DATE(main.submission_timestamp) <= DATE_ADD({{experiment.start}}, {{window.end}} DAY)
